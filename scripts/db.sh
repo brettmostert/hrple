059a1a998109e194e21cd4793554f9d78f448f8d
Brett Mostert <brettmostert@gmail.com>
2022-06-12 08:25:11 +0000

    pg-docker: create multiple dbs

--- scripts/db.sh
+++ scripts/db.sh
@@ -2,16 +2,21 @@
 
 username="user"
 password="password"
-db="hrple"
+dbs="hrple,habit"
 
-while getopts u:p:db: flag
+while getopts u:p:dbs: flag
 do
     case "${flag}" in
         u) username=${OPTARG};;
         p) password=${OPTARG};;
-		db) db=${OPTARG};;
+		dbs) dbs=${OPTARG};;
     esac
 done
-
-docker run --name pg -e POSTGRES_USER=${username} -e POSTGRES_PASSWORD=${password} -e POSTGRES_DB=${db} -p 5432:5432 -d --rm postgres
+docker run --name pg \
+-e POSTGRES_USER=${username} \
+-e POSTGRES_PASSWORD=${password} \
+-e POSTGRES_MULTIPLE_DATABASES=${dbs} \
+-p 5432:5432 \
+-v $(pwd)/scripts/pg-init-scripts:/docker-entrypoint-initdb.d \
+-d --rm postgres
 exit 0
